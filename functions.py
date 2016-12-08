ff2c5f0c1b8efc87b093ab33434f99781cbd9f54
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-07 21:18:10 -0800

    wrote string reversal function utilizing slicing

--- functions.py
+++ functions.py
@@ -22,3 +22,12 @@ def rearrange(array):
 
 string_array2 = ['how', 'now', 'brown','cow', 'bow', 'plow', 'wow', 'clown']
 rearrange(string_array2)
+
+# string reversal via slicing
+def reverse1(string):
+    reverse_string = string[::-1]
+    print reverse_string
+    return reverse_string
+
+string1 = "the quick brown fox jumps over the lazy dog"
+reverse1(string1)
